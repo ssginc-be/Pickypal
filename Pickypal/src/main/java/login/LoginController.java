6ac1d94fb54f83913249070d44c4a1e76146d9b1
HeeJava <heejaeq@gmail.com>
2024-11-28 19:10:04 +0900

    feat: Add client login UI (#6)

---
+++ Pickypal/src/main/java/login/LoginController.java
@@ -0,0 +1,42 @@
+package login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    private double x = 0,y = 0;
+
+    @FXML
+    private AnchorPane sideBar;
+
+    private Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sideBar.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+
+        sideBar.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+    }
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+    @FXML
+    void closeProgram(ActionEvent event) {
+        stage.close();
+    }
+}
