d00571b95737c9f72cba8a2125077be2dc3d95cf
Queue-ri <qriositylog@gmail.com>
2024-11-29 11:09:45 +0900

    feat: Add backend main file

---
+++ api/src/main/java/com/pickypal/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.pickypal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.io.IOException;
+
+/**
+ * @author Queue-ri
+ */
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ApiApplication {
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
