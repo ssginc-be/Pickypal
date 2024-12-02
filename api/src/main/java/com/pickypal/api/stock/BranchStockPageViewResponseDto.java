27848381abd0f83beaee4ffd5d29bc64cc47168f
Queue-ri <qriositylog@gmail.com>
2024-12-02 19:22:53 +0900

    feat: Add backend branch stock view dto (#39)

---
+++ api/src/main/java/com/pickypal/api/stock/BranchStockPageViewResponseDto.java
@@ -0,0 +1,19 @@
+package com.pickypal.api.stock;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author Queue-ri
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BranchStockPageViewResponseDto {
+    private List<BranchStock> content;
+    private Integer currentPage;
+}
