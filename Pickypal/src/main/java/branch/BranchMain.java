94a485db264eef68d2f1b4367513c140cf53b6c1
Hyun7en <rhqudgus99@gmail.com>
2024-12-02 14:36:13 +0900

    feat: Modify client main (#4)

--- Pickypal/src/main/java/branch/BranchMain.java
+++ Pickypal/src/main/java/branch/BranchMain.java
@@ -6,30 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class BranchMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/branchManage.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/branchOrdersManage.fxml"));
         // fxml/testFX.fxml을 불러온다.
         Parent root = loader.load();
 
-        BranchManageController controller = loader.getController();
-
-        // 데이터 추가
-        controller.addOrder("ORD004", "PRD004", "서울", "2024-11-01", "2024-11-05");
-        controller.addOrder("ORD005", "PRD005", "부산", "2024-11-02", "2024-11-06");
-
-        // 기본 타이틀바 제거
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        BranchOrdersController controller = loader.getController();
 
         // GUI에 보일 프로그램 제목을 설정한다.
         primaryStage.setTitle("Pickypal");
 
-        // 화면을 설정한다. 두번째 파라미터는 프로그램의 Width, 세번째는 Height를 설정한다
-        primaryStage.setScene(new Scene(root, 1024, 768));
+        // fxml 파일에 지정된 화면 크기에 따른 UI 보이게.
+        primaryStage.setScene(new Scene(root));
 
-        // 화면을 보여주는 작업을 실행한다
+        // 화면을 보여주는 작업 실행.
         primaryStage.show();
     }
 
