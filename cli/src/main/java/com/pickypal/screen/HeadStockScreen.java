279273c785fbc2217772368aae8e57335580644c
Queue-ri <qriositylog@gmail.com>
2024-12-04 03:43:54 +0900

    feat: Add client login/signup/head stock screen files (#66)

---
+++ cli/src/main/java/com/pickypal/screen/HeadStockScreen.java
@@ -0,0 +1,112 @@
+package com.pickypal.screen;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.pickypal.dto.auth.LoginResponseDto;
+import com.pickypal.dto.auth.SignUpRequestDto;
+import com.pickypal.dto.stock.HeadStockViewResponseDto;
+import com.pickypal.util.ApiKit;
+import com.pickypal.util.ApiResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Queue-ri
+ */
+
+public class HeadStockScreen {
+
+    public static void start(LoginResponseDto loginInfo) throws IOException {
+        //Runtime.getRuntime().exec("cls"); // for Windows
+        Scanner sc = new Scanner(System.in);
+
+        boolean typedInvalidMenu = false;
+        int currentOption = 0;
+        int pageIdx = 0;
+        String value = "";
+
+        while(true) {
+            System.out.println("[ [본사] 재고 조회 화면 ]");
+            if (loginInfo != null) System.out.println("* 로그인 정보: " + loginInfo.getUserName() + " / " + loginInfo.getRole());
+            System.out.println("---------------------------------------------------------");
+            System.out.printf("%10s%10s", "상품ID", "상품명\n");
+            System.out.println("---------------------------------------------------------");
+
+            getByPage(pageIdx, currentOption, value, loginInfo.getAccessToken());
+
+            System.out.println("---------------------------------------------------------");
+
+            System.out.println();
+            System.out.println("0) 전체 조회");
+            System.out.println("1) 상품 id 조회");
+            System.out.println("2) 상품명 조회");
+            System.out.println("3) 상품 유형 조회");
+            System.out.println("4) 최근 입고일 조회");
+            System.out.println("5) [← 뒤로 가기]");
+
+            System.out.println();
+            System.out.print("* * * 조회 옵션 번호를 선택해주세요: ");
+            currentOption = sc.nextInt();
+
+            if (currentOption == 3) {
+                System.out.print("* * * 유형 선택: 0)행사 상품  1)차별화 상품  2)Fresh Food >> ");
+                int selected = sc.nextInt();
+                switch (selected) {
+                    case 0: value = "행사 상품"; break;
+                    case 1: value = "차별화 상품"; break;
+                    case 2: value = "Fresh Food"; break;
+                }
+            }
+            else if (currentOption == 5) {
+                return;
+            }
+            else if (currentOption != 0) {
+                sc.nextLine(); // remove line feed
+                System.out.print("* * * 검색할 값 >> ");
+                value = sc.nextLine();
+            }
+
+            System.out.print("* * * 조회할 페이지 번호 >> ");
+            pageIdx = sc.nextInt();
+        }
+
+    }
+
+    public static void getByPage(int pageIdx, int option, String value, String accessToken) throws JsonProcessingException, UnsupportedEncodingException {
+        // API 호출
+        ApiKit apiKit = new ApiKit();
+        String endpoint = "";
+        String encodedValue = URLEncoder.encode(value, "UTF-8");
+
+        switch (option) {
+            case 0: endpoint = "http://localhost:8080/head/stock/" + pageIdx; break;
+            case 1: endpoint = "http://localhost:8080/head/stock/item-id?item_id=" + encodedValue; break;
+            case 2: endpoint = "http://localhost:8080/head/stock/item-name/" + pageIdx + "?item_name=" + encodedValue; break;
+            case 3: endpoint = "http://localhost:8080/head/stock/type/" + pageIdx + "?type=" + encodedValue; break;
+            case 4: endpoint = "http://localhost:8080/head/stock/date/" + pageIdx + "?date=" + encodedValue; break;
+        }
+
+        ApiResponse response = apiKit.getRequestWithAuth(endpoint, accessToken);
+
+        if (response.getStatusCode() == 200) { // 정상 호출되면
+            // json string을 dto 객체의 List로 파싱
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule()); // LocalDateTime 파싱을 위해 필요
+            String jsonStr = response.getJsonStr();
+            List<HeadStockViewResponseDto> dtoList = mapper.readValue(jsonStr, TypeFactory.defaultInstance().constructCollectionType(List.class, HeadStockViewResponseDto.class));
+            for (HeadStockViewResponseDto dto : dtoList) {
+                System.out.printf("%9s%15s%22s%9s%9s%9s%9s%9s%29s", dto.getStockId(), dto.getItemId(), dto.getItemName(), dto.getType(), dto.getTag(), dto.getSupplierName(), dto.getPrice(), dto.getStock(), dto.getIncomingTime());
+                System.out.println();
+            }
+        }
+        else { // 호출 실패하면
+            System.out.println("* * * HeadStockScreen: API failed");
+        }
+    }
+}
--- cli/src/main/java/com/pickypal/screen/SignUpScreen.java
+++ cli/src/main/java/com/pickypal/screen/SignUpScreen.java
@@ -1,7 +1,12 @@
 package com.pickypal.screen;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pickypal.dto.auth.LoginRequestDto;
+import com.pickypal.dto.auth.LoginResponseDto;
+import com.pickypal.dto.auth.SignUpRequestDto;
 import com.pickypal.util.ApiKit;
-import lombok.RequiredArgsConstructor;
+import com.pickypal.util.ApiResponse;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -11,10 +16,9 @@ import java.util.Scanner;
  */
 
 public class SignUpScreen {
-    private ApiKit apiKit = new ApiKit();
 
-    public void start() throws IOException {
-        Runtime.getRuntime().exec("cls"); // for Windows
+    public static void start() throws IOException {
+        //Runtime.getRuntime().exec("cls"); // for Windows
 
         System.out.println("[ 회원가입 화면 ]");
         Scanner sc = new Scanner(System.in);
@@ -28,14 +32,40 @@ public class SignUpScreen {
         System.out.print("* 이메일: ");
         String email = sc.nextLine();
 
-        System.out.print("* 비밀번호: ");
-        String pw = sc.nextLine();
+        String pw = "";
+        boolean invalidPw = true;
+        while(invalidPw) {
+            System.out.print("* 비밀번호: ");
+            String input = sc.nextLine();
+            if (input.length() > 5) {
+                invalidPw = false;
+                pw = input;
+            }
+            else {
+                System.out.println("* * * 패스워드는 6자리 이상이어야 합니다.");
+            }
+        }
 
-        signUp(uid, name, email, pw);
+        System.out.print("* * * 지점주로 가입하시겠습니까? (y/n): ");
+        String option = sc.nextLine().toLowerCase();
+
+        String branchId, role;
+        if (option.equals("y")) {
+            System.out.print("* 지점 코드: ");
+            branchId = sc.nextLine();
+            role = "BRANCH";
+        }
+        else {
+            branchId = null;
+            role = "HEAD";
+        }
+
+        signUp(uid, name, email, pw, role, branchId);
     }
 
-    public void signUp(String uid, String name, String email, String pw) {
-        final String SERVER = "http://localhost:8080";
-        apiKit.getRequest(SERVER + "/auth/signup");
+    public static void signUp(String uid, String name, String email, String pw, String role, String bid) {
+        ApiKit apiKit = new ApiKit();
+        SignUpRequestDto reqDto = new SignUpRequestDto(uid, name, email, pw, role, bid);
+        ApiResponse response = apiKit.postRequest("http://localhost:8080/auth/signup", reqDto);
     }
 }
