7a42d5a2cd52b36787eaab9b32a9d1838de35820
Queue-ri <qriositylog@gmail.com>
2024-12-04 00:35:14 +0900

    test: Add client dto list api parse sample code (#63)
    - 본사 재고 조회 예제 코드

--- cli/src/main/java/com/pickypal/util/ApiKit.java
+++ cli/src/main/java/com/pickypal/util/ApiKit.java
@@ -1,22 +1,27 @@
 package com.pickypal.util;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * @author Queue-ri
  */
 
-@Slf4j
 public class ApiKit {
 
     // Http GET request to backend server
-    // **no auth header: 회원가입, 로그인 용도**
+    // **no auth header**
     public ApiResponse getRequest(String endpoint) {
         ApiResponse res = new ApiResponse();
 
@@ -64,13 +69,17 @@ public class ApiKit {
             int statusCode = response.getStatusLine().getStatusCode();
             res.setStatusCode(statusCode);
 
-            // Response 출력
+            // response 출력
             String body;
             if (statusCode == 200) {
-                ResponseHandler<String> handler = new BasicResponseHandler();
-                body = handler.handleResponse(response);
+                // response body를 UTF-8로 파싱
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+                body = bufferedReader.readLine();
+                bufferedReader.close();
+                
                 System.out.println("* * * ApiKit(auth GET): 200 OK");
                 System.out.println(body);
+
             } else {
                 System.out.println("* * * ApiKit(auth GET): Server returned " + statusCode);
                 body = null;
@@ -85,10 +94,80 @@ public class ApiKit {
         return res;
     }
 
+    // Http POST request to backend server
+    // **no auth header: 로그인, 회원가입 용도**
+    public ApiResponse postRequest(String endpoint, Object dto) {
+        ApiResponse res = new ApiResponse();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost postRequest = new HttpPost(endpoint); // POST 메소드 URL 생성
+            postRequest.setHeader("Content-Type", "application/json");
+            // add body
+            String json = mapper.writeValueAsString(dto);
+            HttpEntity entity = new ByteArrayEntity(json.getBytes("UTF-8"));
+            postRequest.setEntity(entity);
+
+            CloseableHttpResponse response = client.execute(postRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            res.setStatusCode(statusCode);
+
+            // Response 출력
+            String body;
+            if (statusCode == 200) {
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                body = handler.handleResponse(response);
+                System.out.println("* * * ApiKit(POST): 200 OK");
+                System.out.println(body);
+            } else {
+                System.out.println("* * * ApiKit(POST): Server returned " + statusCode);
+                body = null;
+            }
+            res.setJsonStr(body);
+            client.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return res;
+    }
+
     // Http POST request **with auth header** to backend server
     // 모든 등록 용도: access token 필요
-    public void postRequestWithAuth(String endpoint) {
+    public ApiResponse postRequestWithAuth(String endpoint) {
+        String ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyb290Iiwicm9sZSI6IkhFQUQiLCJleHAiOjE3MzU3NDQ0NjN9.4Y6yReg_4HO_66rz5e-7XGrQEux80gxf0RQu9ONG598";
+        ApiResponse res = new ApiResponse();
 
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost postRequest = new HttpPost(endpoint); // POST 메소드 URL 생성
+            postRequest.addHeader("Authorization", ACCESS_TOKEN); // token 이용시
+
+            CloseableHttpResponse response = client.execute(postRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            res.setStatusCode(statusCode);
+
+            // Response 출력
+            String body;
+            if (statusCode == 200) {
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                body = handler.handleResponse(response);
+                System.out.println("* * * ApiKit(auth POST): 200 OK");
+                System.out.println(body);
+            } else {
+                System.out.println("* * * ApiKit(auth POST): Server returned " + statusCode);
+                body = null;
+            }
+            res.setJsonStr(body);
+            client.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return res;
     }
 
     // Http DELETE request **with auth header** to backend server
